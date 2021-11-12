6f3df49f232e1b243b3201b9c98a2f7264a697ba
Nicolas LEFEBVRE <nicolas.lefebvre.lp@gmail.com>
2021-11-11 21:52:05 -0500

    Initial Commit

---
+++ MWNext-BLE-remote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MWNext-BLE-remote
+//
+//  Created by Nicolas LEFEBVRE on 11/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MWNext-BLE-remote/MWNext_BLE_remoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MWNext_BLE_remoteApp.swift
+//  MWNext-BLE-remote
+//
+//  Created by Nicolas LEFEBVRE on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MWNext_BLE_remoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
